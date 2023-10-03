f7e735d91cc0eb68679b4a5760d9a72664f6e811
gabri <gabri@PC-PGAME>
2023-10-02 22:34:33 -0300

    Adicionar arquivos de projeto.

---
+++ crudeFornecedores/Controllers/ControllerFornecedor.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using crudeFornecedores.Models;
+using Microsoft.AspNetCore.Mvc;
+using static crudeFornecedores.Controllers.ControllerFornecedor;
+
+namespace crudeFornecedores.Controllers
+{
+    public class ControllerFornecedor : Controller
+    {
+
+        private List<Fornecedor> Fornecedores = new List<Fornecedor>
+        {
+          
+            new Fornecedor { Id = 1, RazaoSocial = "Empresa A", NomeFantasia = "Empresa A", Email = "empresaA@email.com", Telefone = "123-456-7890", EnderecoCompleto = "Endereço A", PessoaContato = "Contato A" },
+            new Fornecedor { Id = 2, RazaoSocial = "Empresa B", NomeFantasia = "Empresa B", Email = "empresaB@email.com", Telefone = "987-654-3210", EnderecoCompleto = "Endereço B", PessoaContato = "Contato B" }
+        };
+
+        internal class Fornecedor
+        {
+            internal int Id;
+            internal string RazaoSocial;
+            internal string NomeFantasia;
+            internal string Email;
+            internal string Telefone;
+            internal string EnderecoCompleto;
+            internal string PessoaContato;
+
+            internal static object FirstOrDefault(Func<object, bool> value)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        
+        public ActionResult Index(object fornecedores)
+        {
+            return ViewResult(fornecedores);
+        }
+
+        private ActionResult ViewResult(object fornecedores)
+        {
+            throw new NotImplementedException();
+        }
+
+        private ActionResult View(object fornecedores)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+
+        
+        public ActionResult Edit(int id)
+        {
+            
+           var fornecedor = Fornecedor.FirstOrDefault(f => f.Id == id);
+            if (fornecedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fornecedor);
+        }
+
+        private ActionResult HttpNotFound()
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        [HttpPost]
+
+       
+        public ActionResult Delete(int id)
+        {
+            
+            var fornecedor = Fornecedores.FirstOrDefault(f => f.Id == id);
+            if (fornecedor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(fornecedor);
+        }
+
+        
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ crudeFornecedores/Models/Fornecedores.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crudeFornecedores.Models
+{
+    public class Fornecedores
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string RazaoSocial { get; set; }
+
+        [Required]
+        public string NomeFantasia { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        public string EnderecoCompleto { get; set; }
+
+        [Required]
+        public string PessoaContato { get; set; }
+    }
+}
